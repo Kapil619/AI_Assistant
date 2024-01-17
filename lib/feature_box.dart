eeb7ce12385553d3c135852ef4f2aa8ddd6724b9
kapil619 <kapilbadokar321@gmail.com>
2024-01-17 23:12:39 +0530

    Finished the UI

---
+++ lib/feature_box.dart
@@ -0,0 +1,55 @@
+import "package:ai_assistant/pallete.dart";
+import "package:flutter/material.dart";
+
+class FeatureBox extends StatelessWidget {
+  final Color color;
+  final String headerText;
+  final String descriptionText;
+  const FeatureBox(
+      {super.key,
+      required this.descriptionText,
+      required this.headerText,
+      required this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 35, vertical: 10),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: const BorderRadius.all(
+          Radius.circular(15),
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 20).copyWith(left: 15),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                headerText,
+                style: const TextStyle(
+                    fontFamily: 'Cera Pro',
+                    color: Pallete.blackColor,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            const SizedBox(height: 3),
+            Padding(
+              padding: const EdgeInsets.only(right: 18.0),
+              child: Text(
+                descriptionText,
+                style: const TextStyle(
+                  fontFamily: 'Cera Pro',
+                  color: Pallete.blackColor,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:ai_assistant/feature_box.dart';
 import 'package:ai_assistant/pallete.dart';
 import 'package:flutter/material.dart';
 
@@ -65,6 +66,47 @@ class _HomePageState extends State<HomePage> {
                     fontFamily: 'Cera Pro'),
               ),
             ),
+          ),
+          //suggestions
+          Container(
+            padding: const EdgeInsets.all(10),
+            margin: const EdgeInsets.only(top: 10, left: 22),
+            alignment: Alignment.centerLeft,
+            child: const Text(
+              'Here are a few features you can try:',
+              style: TextStyle(
+                  fontFamily: 'Cera Pro',
+                  color: Pallete.mainFontColor,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          const Column(
+            children: [
+              FeatureBox(
+                color: Pallete.firstSuggestionBoxColor,
+                headerText: 'ChatGPT',
+                descriptionText:
+                    'A smarter way to stay organized and informed with an AI',
+              ),
+              FeatureBox(
+                color: Pallete.firstSuggestionBoxColor,
+                headerText: 'Dall-E',
+                descriptionText:
+                    'Get inspired and stay creative with your personal assistant p0owered by Dall-E',
+              ),
+              FeatureBox(
+                color: Pallete.firstSuggestionBoxColor,
+                headerText: 'Smart Voice Assistant',
+                descriptionText:
+                    'Get the best of both world with a voice assistant powered by Dall-E and ChatGPT',
+              ),
+            ],
+          ),
+          FloatingActionButton(
+            backgroundColor: Pallete.firstSuggestionBoxColor,
+            onPressed: () {},
+            child: const Icon(Icons.mic),
           ),
         ],
       ),
